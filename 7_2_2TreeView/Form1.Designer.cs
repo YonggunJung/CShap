46a2fa67f1fbf5399eefebef902d8ea58caa2f71
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-01 09:10:09 +0900

    Tree View

---
+++ 7_2_2TreeView/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace _7_2_2TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("IT도서");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tbNode = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExpandAll = new System.Windows.Forms.Button();
+            this.btnCollapseAll = new System.Windows.Forms.Button();
+            this.btnChkDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(36, 46);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Root";
+            treeNode1.Text = "IT도서";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(187, 301);
+            this.treeView1.TabIndex = 0;
+            // 
+            // tbNode
+            // 
+            this.tbNode.Location = new System.Drawing.Point(323, 46);
+            this.tbNode.Name = "tbNode";
+            this.tbNode.Size = new System.Drawing.Size(100, 21);
+            this.tbNode.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(323, 73);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(323, 102);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnExpandAll
+            // 
+            this.btnExpandAll.Location = new System.Drawing.Point(323, 131);
+            this.btnExpandAll.Name = "btnExpandAll";
+            this.btnExpandAll.Size = new System.Drawing.Size(115, 23);
+            this.btnExpandAll.TabIndex = 4;
+            this.btnExpandAll.Text = "확장";
+            this.btnExpandAll.UseVisualStyleBackColor = true;
+            this.btnExpandAll.Click += new System.EventHandler(this.BtnExpandAll_Click);
+            // 
+            // btnCollapseAll
+            // 
+            this.btnCollapseAll.Location = new System.Drawing.Point(323, 162);
+            this.btnCollapseAll.Name = "btnCollapseAll";
+            this.btnCollapseAll.Size = new System.Drawing.Size(115, 23);
+            this.btnCollapseAll.TabIndex = 5;
+            this.btnCollapseAll.Text = "축소";
+            this.btnCollapseAll.UseVisualStyleBackColor = true;
+            this.btnCollapseAll.Click += new System.EventHandler(this.BtnCollapseAll_Click);
+            // 
+            // btnChkDelete
+            // 
+            this.btnChkDelete.Location = new System.Drawing.Point(323, 192);
+            this.btnChkDelete.Name = "btnChkDelete";
+            this.btnChkDelete.Size = new System.Drawing.Size(115, 23);
+            this.btnChkDelete.TabIndex = 6;
+            this.btnChkDelete.Text = "체크된것만 삭제";
+            this.btnChkDelete.UseVisualStyleBackColor = true;
+            this.btnChkDelete.Click += new System.EventHandler(this.BtnChkDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChkDelete);
+            this.Controls.Add(this.btnCollapseAll);
+            this.Controls.Add(this.btnExpandAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbNode);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox tbNode;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExpandAll;
+        private System.Windows.Forms.Button btnCollapseAll;
+        private System.Windows.Forms.Button btnChkDelete;
+    }
+}
+
---
+++ 7_2_2TreeView/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_2_2TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // 텍슨트 박스 값을 가진 TreeNode 객체 생성
+            TreeNode node1 = new TreeNode(tbNode.Text);
+            // 트리 뷰에서 선택된 노드가 있는지 SelectedNode를 통해 체크
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.IsSelected)
+                treeView1.SelectedNode.Nodes.Add(node1);    //선택된 노드가 있으면 노드하위에 Add()
+            else treeView1.Nodes.Add(node1);    // 없으면 노드에 Add()
+            tbNode.Clear();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            //트리 뷰에서 선택된 노드가 있는지 SelectedNode를 통해 체크
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.IsSelected)
+                treeView1.SelectedNode.Remove();    // 있으면 Remove()
+        }
+
+        private void BtnExpandAll_Click(object sender, EventArgs e)
+        {
+            //모든 트리를 확장해서 보여줌
+            treeView1.ExpandAll();
+        }
+
+        private void BtnCollapseAll_Click(object sender, EventArgs e)
+        {
+            //모든 트리를 축소해서 보여줌
+            treeView1.CollapseAll();
+        }
+
+        
+        void RemoveCheckedNodes(TreeNodeCollection nodes)
+        {
+            //체크한 노드만 삭제하기 위한 기능
+            //전달인자로 TreeNodeCollection 객체를 전달
+            List<TreeNode> chkNodes = new List<TreeNode>();
+            foreach (TreeNode node in nodes)
+            {
+                //노드가 체크인지 확인해서 맞으면 chkNodes에 추가
+                if (node.Checked) chkNodes.Add(node);
+                else RemoveCheckedNodes(node.Nodes);    // 아니면 하위노드 확인
+            }
+
+            // chkNodes을 처음부터 마지막까지 확인해서 체크노드 삭제
+            foreach (TreeNode chknode in chkNodes)
+            {
+                nodes.Remove(chknode);
+            }
+        }
+
+        private void BtnChkDelete_Click(object sender, EventArgs e)
+        {
+            // 버튼 클릭시 메소드 호출해서 삭제
+            RemoveCheckedNodes(treeView1.Nodes);
+        }
+    }
+}
