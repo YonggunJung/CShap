c4c37ecaa7f1e2fcfe2a3caba7d1b59b44fcb15b
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-13 08:55:25 +0900

    Add files via upload

---
+++ 8_2_9serializeEx/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace _8_2_9serializeEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.btnDeserialize = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbMobile = new System.Windows.Forms.TextBox();
+            this.tbEmali = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(439, 89);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(136, 93);
+            this.btnSerialize.TabIndex = 0;
+            this.btnSerialize.Text = "직렬화";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.BtnSerialize_Click);
+            // 
+            // btnDeserialize
+            // 
+            this.btnDeserialize.Location = new System.Drawing.Point(439, 260);
+            this.btnDeserialize.Name = "btnDeserialize";
+            this.btnDeserialize.Size = new System.Drawing.Size(136, 105);
+            this.btnDeserialize.TabIndex = 1;
+            this.btnDeserialize.Text = "역직렬화";
+            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.Click += new System.EventHandler(this.BtnDeserialize_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(105, 67);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(273, 21);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbMobile
+            // 
+            this.tbMobile.Location = new System.Drawing.Point(105, 104);
+            this.tbMobile.Name = "tbMobile";
+            this.tbMobile.Size = new System.Drawing.Size(273, 21);
+            this.tbMobile.TabIndex = 3;
+            // 
+            // tbEmali
+            // 
+            this.tbEmali.Location = new System.Drawing.Point(105, 138);
+            this.tbEmali.Name = "tbEmali";
+            this.tbEmali.Size = new System.Drawing.Size(273, 21);
+            this.tbEmali.TabIndex = 4;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(105, 176);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(273, 21);
+            this.tbAddress.TabIndex = 5;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(105, 278);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(273, 21);
+            this.tbResult.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "휴대폰";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "이메일";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "주소";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "역직렬화 결과";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbEmali);
+            this.Controls.Add(this.tbMobile);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btnDeserialize);
+            this.Controls.Add(this.btnSerialize);
+            this.Name = "Form1";
+            this.Text = "역직렬화 결과";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button btnDeserialize;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbMobile;
+        private System.Windows.Forms.TextBox tbEmali;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 8_2_9serializeEx/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+//BinaryFormatter 클래스를 사용하기 위해 사용하는 네임스페이스
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_2_9serializeEx
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList arrlist;
+        public Form1()
+        {
+            InitializeComponent();
+            arrlist = new ArrayList();  //리스트 생성
+        }
+
+        private void BtnSerialize_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"C:\Users\admin\Desktop\C#과제\serialize.bin",
+                FileMode.Create);       // FileStream 생성모드로 생성
+            Person p = new Person(tbName.Text, tbMobile.Text, tbEmali.Text, tbAddress.Text);
+
+            arrlist.Add(p);     //리스트에 Person객체q 더하기
+
+            BinaryFormatter bf = new BinaryFormatter();     //직렬화 객체 생성
+            bf.Serialize(fs, arrlist);  //arrlist를 직렬화
+            fs.Close();
+            arrlist.Clear();
+        }
+
+        private void BtnDeserialize_Click(object sender, EventArgs e)
+        {
+            tbResult.Text = "";
+            FileStream fs = new FileStream(@"C:\Users\admin\Desktop\C#과제\Serialize.bin", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();     //역직렬화 객체 생성
+            arrlist = (ArrayList)bf.Deserialize(fs);    //fs를 역직렬화 후 리스트화해서 저장
+
+            foreach(Person p in arrlist)
+            {
+                tbResult.Text += p.GetName() + "\n" +
+                    p.GetMobile() + "\n" +
+                    p.GetEmail() + "\n" +
+                    p.GetAddress() + "\n";
+            }
+            fs.Close();
+        }
+    }
+    [Serializable]      //Person 클래스를 직렬화하겠다는 의미
+    class Person 
+    {
+        string name;
+        string mobile;
+        string email;
+        [NonSerialized]     //address만 역직렬화하겠다는 의미
+        string address;
+
+        public Person(string aName, string aMobile, string aEmail, string aAddress)
+        {
+            name = aName;
+            mobile = aMobile;
+            email = aEmail;
+            address = aAddress;
+        }
+
+        public string GetName() { return name; }
+        public string GetMobile() { return mobile; }
+        public string GetEmail() { return email; }
+        public string GetAddress() { return address; }
+    }
+
+}
