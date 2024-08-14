44bb3e08bd70f995441763afeb39935ca5f3e9c6
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-14 10:08:58 +0900

    Add files via upload

---
+++ 9_1_3ThreadInfo/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _9_1_3ThreadInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbThreadInfo = new System.Windows.Forms.TextBox();
+            this.btnThreadInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbThreadInfo
+            // 
+            this.tbThreadInfo.Location = new System.Drawing.Point(58, 23);
+            this.tbThreadInfo.Multiline = true;
+            this.tbThreadInfo.Name = "tbThreadInfo";
+            this.tbThreadInfo.Size = new System.Drawing.Size(319, 232);
+            this.tbThreadInfo.TabIndex = 0;
+            // 
+            // btnThreadInfo
+            // 
+            this.btnThreadInfo.Location = new System.Drawing.Point(109, 288);
+            this.btnThreadInfo.Name = "btnThreadInfo";
+            this.btnThreadInfo.Size = new System.Drawing.Size(232, 61);
+            this.btnThreadInfo.TabIndex = 1;
+            this.btnThreadInfo.Text = "스레드 정보";
+            this.btnThreadInfo.UseVisualStyleBackColor = true;
+            this.btnThreadInfo.Click += new System.EventHandler(this.BtnThreadInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThreadInfo);
+            this.Controls.Add(this.tbThreadInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbThreadInfo;
+        private System.Windows.Forms.Button btnThreadInfo;
+    }
+}
+
---
+++ 9_1_3ThreadInfo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _9_1_3ThreadInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThreadInfo_Click(object sender, EventArgs e)
+        {
+            //Thread 클래스를 이용하여 객체를 선언하되,
+            //CurrentThread를 통해 헌재 주 스레드 객체를 얻어온다
+            Thread th = Thread.CurrentThread;
+            th.Name = "threadInfo";     //스레드 이름 설정
+            //스레드 해시코드 출력. 해시코드는 객체 식별하는 하나의 정수 값
+            tbThreadInfo.Text += "HashCode : " + th.GetHashCode() + "\r\n";
+            tbThreadInfo.Text += "스레드 이름 :" + th.Name + "\r\n";
+            tbThreadInfo.Text += "스레드 우선순위 :" + th.Priority + "\r\n";
+            tbThreadInfo.Text += "스레드 상태 :" + th.ThreadState + "\r\n";
+        }
+    }
+}
