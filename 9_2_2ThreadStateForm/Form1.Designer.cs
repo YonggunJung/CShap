f5e4f7ae1217513244bf65ba7a97cda7a4a5374f
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-23 17:22:59 +0900

    ThreadStateForm

---
+++ 9_2_2ThreadStateForm/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _9_2_2ThreadStateForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbThreadState = new System.Windows.Forms.TextBox();
+            this.btnThreadState = new System.Windows.Forms.Button();
+            this.btnSuspend = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbThreadState
+            // 
+            this.tbThreadState.Location = new System.Drawing.Point(43, 37);
+            this.tbThreadState.Multiline = true;
+            this.tbThreadState.Name = "tbThreadState";
+            this.tbThreadState.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbThreadState.Size = new System.Drawing.Size(311, 256);
+            this.tbThreadState.TabIndex = 0;
+            // 
+            // btnThreadState
+            // 
+            this.btnThreadState.Location = new System.Drawing.Point(43, 330);
+            this.btnThreadState.Name = "btnThreadState";
+            this.btnThreadState.Size = new System.Drawing.Size(102, 23);
+            this.btnThreadState.TabIndex = 1;
+            this.btnThreadState.Text = "스레드 시작";
+            this.btnThreadState.UseVisualStyleBackColor = true;
+            this.btnThreadState.Click += new System.EventHandler(this.BtnThreadState_Click);
+            // 
+            // btnSuspend
+            // 
+            this.btnSuspend.Location = new System.Drawing.Point(162, 330);
+            this.btnSuspend.Name = "btnSuspend";
+            this.btnSuspend.Size = new System.Drawing.Size(75, 23);
+            this.btnSuspend.TabIndex = 2;
+            this.btnSuspend.Text = "일시정지";
+            this.btnSuspend.UseVisualStyleBackColor = true;
+            this.btnSuspend.Click += new System.EventHandler(this.BtnSuspend_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(279, 330);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "다시시작";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.BtnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSuspend);
+            this.Controls.Add(this.btnThreadState);
+            this.Controls.Add(this.tbThreadState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbThreadState;
+        private System.Windows.Forms.Button btnThreadState;
+        private System.Windows.Forms.Button btnSuspend;
+        private System.Windows.Forms.Button btnResume;
+    }
+}
+
---
+++ 9_2_2ThreadStateForm/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_2_2ThreadStateForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Thread thd;
+        private string strState;
+        //Invoke( ）의 전달인자로 사용할 델리게이트 CrossCall()을 선언
+        delegate void CrossCall();
+        private void BtnThreadState_Click(object sender, EventArgs e)
+        {
+            thd = new Thread(new ThreadStart(ThreadFunction));
+            tbThreadState.Text += "스레드 식별 : " + thd.GetHashCode() + "\r\n";
+            tbThreadState.Text += "스레드 상태 : " + thd.ThreadState + "\r\n";
+            thd.Start();
+            tbThreadState.Text += "스레드 상태 : " + thd.ThreadState + "\r\n";
+        }
+        private void ThreadFunction()
+        {
+            int count = 0;
+            while (count < 100)
+            {
+                //스레드 안에서 트롤에 접근하면 크로스 스레드 오류가 발생
+                //|nvoke( ) 메소드의 전달인자인 CrossCall() 델리게이트를 통해
+                //가리키는 메소드 ThreadState를 호출
+                tbThreadState.Invoke(new CrossCall(ThreadState));
+                //스레드의 상태를 1 초마다 텍스트 박스에출력
+                count++;
+                Thread.Sleep(1000);
+                tbThreadState.Text += "스레드 상태: " + thd.ThreadState + "\r\n";
+            }
+        }
+
+        private void ThreadState()
+        {
+            //스레드의 상태를 문자열로 저장한 변수 strState를 텍스트 박스에 출력
+            tbThreadState.Text += strState;
+        }
+
+        private void BtnSuspend_Click(object sender, EventArgs e)
+        {
+            thd.Suspend();
+            tbThreadState.Text += "스레드 상태 : " + thd.ThreadState + "\r\n";
+        }
+
+        private void BtnResume_Click(object sender, EventArgs e)
+        {
+            thd.Resume();
+            tbThreadState.Text += "스레드 상태 :" + thd.ThreadState + "\r\n";
+        }
+    }
+}
