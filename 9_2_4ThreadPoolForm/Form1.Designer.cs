9f691315f4ec6035b4a2422d52b6f71d77de7d70
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-28 17:10:23 +0900

    ThreadPoolForm

---
+++ 9_2_4ThreadPoolForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _9_2_4ThreadPoolForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbThreadState = new System.Windows.Forms.TextBox();
+            this.btnThreadPool = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbThreadState
+            // 
+            this.tbThreadState.Location = new System.Drawing.Point(39, 27);
+            this.tbThreadState.Multiline = true;
+            this.tbThreadState.Name = "tbThreadState";
+            this.tbThreadState.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbThreadState.Size = new System.Drawing.Size(284, 286);
+            this.tbThreadState.TabIndex = 0;
+            // 
+            // btnThreadPool
+            // 
+            this.btnThreadPool.Location = new System.Drawing.Point(68, 345);
+            this.btnThreadPool.Name = "btnThreadPool";
+            this.btnThreadPool.Size = new System.Drawing.Size(196, 51);
+            this.btnThreadPool.TabIndex = 1;
+            this.btnThreadPool.Text = "스레드풀 생성";
+            this.btnThreadPool.UseVisualStyleBackColor = true;
+            this.btnThreadPool.Click += new System.EventHandler(this.BtnThreadPool_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThreadPool);
+            this.Controls.Add(this.tbThreadState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbThreadState;
+        private System.Windows.Forms.Button btnThreadPool;
+    }
+}
+
---
+++ 9_2_4ThreadPoolForm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_2_4ThreadPoolForm
+{
+    public partial class Form1 : Form
+    {
+        string thdCode = null; //생성된 스레드의 상태 정보를 저장하기 위한 변수
+        delegate void CrossCall();  //Invoke() 전달인자로 사용할 델리게이트 CrossCall() 선언
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThreadPool_Click(object sender, EventArgs e)
+        {
+            tbThreadState.Clear();
+            tbThreadState.Text += "스레드풀 시작" + "\r\n";
+            //스레드폴 메소드인 QueueUserWorkltem() 이용하여
+            //10개의 레드를 생성하고 관리
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadFunction), i);
+            }
+            tbThreadState.Text += "스레드풀 종료" + "\r\n";
+        }
+
+        private void ThreadFunction(object obj)
+        {
+            //스레드 안에서 컨트롤에 접근하면 크로스 스레드 오류가 발생
+            //Invoke()메소드 전달인자인 CrossCall() 델리게이트를 통해
+            //가리키는 메소드 ThreadState를 호출
+            tbThreadState.Invoke(new CrossCall(ThreadState));
+            //생성한 스레드의 해시 코드 및 스레드의 상태표시
+            thdCode = "스레드 식별 : " + Thread.CurrentThread.GetHashCode().ToString() +
+                "스레드 상태 : " + Thread.CurrentThread.ThreadState.ToString() + "\r\n";
+            Thread.Sleep(1000);
+        }
+
+        private void ThreadState()
+        {
+            //스레드 상태 정보를 저장한 문자열을 텍스트 박스에 표시
+            tbThreadState.Text += thdCode;
+        }
+    }
+}
