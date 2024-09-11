36b7bb42b3119a94494785abf321c25276004d32
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-09-11 16:49:53 +0900

    ThreadMutex

---
+++ 9_3_7ThreadMutex/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _9_3_7ThreadMutex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThreadMutex = new System.Windows.Forms.Button();
+            this.tbThreadState = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnThreadMutex
+            // 
+            this.btnThreadMutex.Location = new System.Drawing.Point(47, 298);
+            this.btnThreadMutex.Name = "btnThreadMutex";
+            this.btnThreadMutex.Size = new System.Drawing.Size(239, 64);
+            this.btnThreadMutex.TabIndex = 0;
+            this.btnThreadMutex.Text = "스레드뮤텍스(Mutex)";
+            this.btnThreadMutex.UseVisualStyleBackColor = true;
+            this.btnThreadMutex.Click += new System.EventHandler(this.BtnThreadMutex_Click);
+            // 
+            // tbThreadState
+            // 
+            this.tbThreadState.Location = new System.Drawing.Point(47, 59);
+            this.tbThreadState.Multiline = true;
+            this.tbThreadState.Name = "tbThreadState";
+            this.tbThreadState.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbThreadState.Size = new System.Drawing.Size(239, 204);
+            this.tbThreadState.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbThreadState);
+            this.Controls.Add(this.btnThreadMutex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThreadMutex;
+        private System.Windows.Forms.TextBox tbThreadState;
+    }
+}
+
---
+++ 9_3_7ThreadMutex/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_3_7ThreadMutex
+{
+    public partial class Form1 : Form
+    {
+        private string thdCode;
+        private delegate void CrossCall();
+        private int money;
+        //뮤텍스 객체 생성 new Mutex(생성스레드의 소유권, 뮤텍스 이름)
+        private static Mutex mtx = new Mutex(false, "mtxObj");
+        public Form1()
+        {
+            InitializeComponent();
+            thdCode = null;
+            money = 10000;
+        }
+
+        private void BtnThreadMutex_Click(object sender, EventArgs e)
+        {
+            tbThreadState.Clear();
+            ThreadStart ts1 = new ThreadStart(ThreadTeller);
+            ThreadStart ts2 = new ThreadStart(ThreadTeller);
+            ThreadStart ts3 = new ThreadStart(ThreadTeller);
+            Thread mobileBanking = new Thread(ts1);
+            Thread internetBanking = new Thread(ts2);
+            Thread teleBanking = new Thread(ts3);
+
+            mobileBanking.Start();
+            internetBanking.Start();
+            teleBanking.Start();
+        }
+
+        private void ThreadTeller()
+        {
+            int task = 0;
+
+            mtx.WaitOne();      //하나의 자원을 이용할 수 있는때까지 대기하라는 신호
+            while (task < 5)
+            {
+                money += 1000;
+                tbThreadState.Invoke(new CrossCall(ThreadState));
+                thdCode = "뱅킹고유번호 " + Thread.CurrentThread.GetHashCode() +
+                    ": 입금잔액 : " + money + "원" + "\r\n";
+                Thread.Sleep(100);
+                task++;
+            }
+            mtx.ReleaseMutex(); //잠금 해제, 다음 스레드가 공유자원 사용
+        }
+        private void ThreadState()
+        {
+            tbThreadState.Text += thdCode;
+        }
+    }
+}
