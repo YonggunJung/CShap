cfa0c0c03446cbe77d8d74a54b3676a00e2746c1
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-09-25 20:48:43 +0900

    CommonDialog

---
+++ 10_2_4CommonDialog/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _10_2_4CommonDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cd = new System.Windows.Forms.ColorDialog();
+            this.fd = new System.Windows.Forms.FontDialog();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.Textbox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(99, 47);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(150, 35);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "열기";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(99, 108);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(131, 36);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(424, 47);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(150, 32);
+            this.btnFont.TabIndex = 2;
+            this.btnFont.Text = "폰트 설정";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.BtnFont_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(443, 108);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(131, 33);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.Text = "색상 설정";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // Textbox1
+            // 
+            this.Textbox1.Location = new System.Drawing.Point(99, 210);
+            this.Textbox1.Multiline = true;
+            this.Textbox1.Name = "Textbox1";
+            this.Textbox1.Size = new System.Drawing.Size(475, 185);
+            this.Textbox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Textbox1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog cd;
+        private System.Windows.Forms.FontDialog fd;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.TextBox Textbox1;
+    }
+}
+
---
+++ 10_2_4CommonDialog/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_2_4CommonDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            // 열기 대화 상자가 모달 형태로 출력
+            //대화 상자 안에서 파일 1개 선택후 열기버튼을 누르면
+            // 해당 파일 경로와 이름이 텍스트 박스에 출력
+            if (ofd.ShowDialog() == DialogResult.OK)
+                Textbox1.Text = ofd.FileName + "파일을 엽니다.";
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            sfd.Filter = "All File(*.*) | (*.*)";
+            //저장 대화 상자가 모달 형태로 출력
+            //대화 상자에 파일 이름 입력후 저장 버튼을 누루면
+            //해당 파일 경로와 이름을 텍스트 박스에 출력
+            if (sfd.ShowDialog() == DialogResult.OK)
+                Textbox1.Text = sfd.FileName + "파일 저장!";
+        }
+
+        private void BtnFont_Click(object sender, EventArgs e)
+        {
+            // 폰트 적용
+            if (fd.ShowDialog() == DialogResult.OK)
+                Textbox1.Font = fd.Font;
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            //색상 적용
+            if (cd.ShowDialog() == DialogResult.OK)
+                Textbox1.ForeColor = cd.Color;
+        }
+    }
+}
